0e58a42c89d6c301b93d340dc5420a11c37af75f
Trace Helms <tracehelms@gmail.com>
2023-05-05 09:52:17 -0700

    Add recursive Fibonacci sequence algorithms

---
+++ src/recursion/__tests__/fibonacci.test.ts
@@ -0,0 +1,61 @@
+import { bottomUpFibonacci, fibonacci, memoizedFibonacci } from '../fibonacci';
+
+describe('fibonacci', function() {
+  test('it works', function() {
+    expect(fibonacci(0)).toBe(0);
+    expect(fibonacci(1)).toBe(1);
+    expect(fibonacci(2)).toBe(1);
+    expect(fibonacci(3)).toBe(2);
+    expect(fibonacci(10)).toBe(55);
+    expect(fibonacci(19)).toBe(4181);
+  });
+
+  test('it doesnt run fast', function() {
+    const t0 = performance.now();
+    const fib = fibonacci(30);
+    const t1 = performance.now();
+    console.log(`Execution time for calculating the 30th Fibonacci took ${t1 - t0} milliseconds.`);
+
+    expect(fib).toEqual(832040);
+  });
+});
+
+describe('memoizedFibonacci', function() {
+  test('it works', function() {
+    expect(memoizedFibonacci(0)).toBe(0);
+    expect(memoizedFibonacci(1)).toBe(1);
+    expect(memoizedFibonacci(2)).toBe(1);
+    expect(memoizedFibonacci(3)).toBe(2);
+    expect(memoizedFibonacci(10)).toBe(55);
+    expect(memoizedFibonacci(19)).toBe(4181);
+  });
+
+  test('it runs fast', function() {
+    const t0 = performance.now();
+    const fib = memoizedFibonacci(30);
+    const t1 = performance.now();
+    console.log(`Execution time for calculating the 30th Fibonacci with memoization took ${t1 - t0} milliseconds.`);
+
+    expect(fib).toEqual(832040);
+  });
+});
+
+describe('bottomUpFibonacci', function() {
+  test('it works', function() {
+    expect(bottomUpFibonacci(0)).toBe(0);
+    expect(bottomUpFibonacci(1)).toBe(1);
+    expect(bottomUpFibonacci(2)).toBe(1);
+    expect(bottomUpFibonacci(3)).toBe(2);
+    expect(bottomUpFibonacci(10)).toBe(55);
+    expect(bottomUpFibonacci(19)).toBe(4181);
+  });
+
+  test('it runs fast', function() {
+    const t0 = performance.now();
+    const fib = bottomUpFibonacci(30);
+    const t1 = performance.now();
+    console.log(`Execution time for calculating the 30th Fibonacci with the bottom up method took ${t1 - t0} milliseconds.`);
+
+    expect(fib).toEqual(832040);
+  });
+});
---
+++ src/recursion/fibonacci.ts
@@ -0,0 +1,64 @@
+/*
+ * Fibonacci Sequence
+ *
+ * The first way is basically an exact code implementation of the definition.
+ * However, it has many redundand recursive call stacks (like calcuating
+ * `fibonacci(4)` again as a part of `fibonacci(5)` and `fibonacci(6)`, etc.
+ *
+ * The efficiency of this algorithm is O(2^n), which is very slow.
+ *
+ */
+export const fibonacci = (n: number): number => {
+  if (n < 2) {
+    return n;
+  }
+
+  return fibonacci(n - 2) + fibonacci(n - 1);
+}
+
+/*
+ * Memoized Fibonacci Sequence
+ *
+ * Adding memoization to this algorithm greatly increases the efficiency at the cost
+ * of additional space to store the memo. This algorithm can look up Fibonacci numbers
+ * that have already been calculated instead of recalculating them.
+ *
+ * The efficiency of this algorithm is O(n), much faster.
+ *
+ */
+export const memoizedFibonacci = (n: number, memo: Map<number, number> = new Map()): number => {
+  if (n < 2) {
+    return n;
+  }
+
+  if (!memo.get(n)) {
+    memo.set(n, memoizedFibonacci(n - 2) + memoizedFibonacci(n - 1));
+  }
+
+  return memo.get(n) || 0;
+}
+
+/*
+ * Bottom Up Fibonacci Sequence
+ *
+ * This non-recursive version is less intuitive, but it's another way to speed up
+ * the Fibonacci sequence. It also doesn't need the extra space like the memoized
+ * version.
+ *
+ * This algorithm is also O(n), which is as fast as the memoized version.
+ *
+ */
+export const bottomUpFibonacci = (n: number): number => {
+  if (n === 0) return 0;
+
+  let secondLast = 0;
+  let last = 1;
+
+  for (let i = 1; i < n; i++) {
+    const temp = secondLast;
+    secondLast = last;
+    last = temp + secondLast;
+  }
+
+  return last;
+}
